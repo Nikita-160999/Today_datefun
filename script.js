b1ea80dbaea504a4457bdac3d828adbb11ce5c03
Nikita-160999 <nikitarabade@icloud.com>
2024-12-18 16:40:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function GetDate(){
+
+    console.log(Date());
+    alert(Date());
+}
+
+function Showdata(){
+    document.write(" <br><br>Greenwich Mean Time (GMT) is the local mean time at the Royal Observatory in Greenwich, London, counted from midnight. At different times in the past, it has been calculated in different ways, including being calculated from noon; as a consequence, it cannot be used to specify a particular time unless a context is given. The term GMT is also used as one of the names for the time zone UTC+00:00 and, in UK law, is the basis for civil time in the United Kingdom.");
+    let img = document.createElement('img');
+    img.src = "Greenwich_clock.jpg";
+}
